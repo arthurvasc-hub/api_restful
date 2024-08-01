cb70ebbaace53c85d597341357a083f96865f2c3
arthurvasc <arthurvasc2@gmail.com>
2024-08-01 12:50:42 -0300

    Testing new update function

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,4 @@
-// Definições de rotas.
+// Definições de rotas para o CRUD.
 import express from "express";
 import { createNewBook, createManyBooks, getAllBooks, getByTitle, updateBookTitle, updateBookAuthor, updateBookSynopsis } from "../services/books";
 import { error } from "console";
@@ -9,27 +9,27 @@ const router = express.Router()
 // Rota onde vou exibir todos os Livros no DB
 router.get('/books', async (req, res) => {
 const result = await getAllBooks()
-res.json({ result })
-});
+    res.json({ result })
+    });
 
 // Rota onde vou exibir um livro no DB, pelo título
 router.get('/book', async (req, res) => {
     const result = await getByTitle('TypeScript for JavaScript Developers')
-if(result){
+    if(result){
     res.status(200).json({ result })
-} else {
+    } else {
     res.status(500).json({ error: "Não existe livro com esse título" })
-}});
+    }});
 
 // Rota para adicionar um livro 
 router.post('/book', async (req, res) => {
 const book = await createNewBook({
     title: "Aprendendo a Linguagem R", author:"Milene Guimarães", synopsis:""});
-if(book){
+    if(book){
     res.status(201).json({book});
-} else {
+    } else {
     res.status(500).json({error: "Título já cadastrado"});
-}});
+    }});
 
 // Rota para adicionar muitos livros
 router.post('/manyBooks', async (req, res) => {
@@ -46,29 +46,29 @@ router.post('/manyBooks', async (req, res) => {
 // Rota para atualizar o Título do livro
 router.put('/bookTitle', async (req, res) => {
     const result = await updateBookTitle(' ',' ')                               // Recebe 2 parâmetros, Título do livro que deseja atualizar e Novo Título para o livro.
-if(result){
+    if(result){
     res.status(200).json({ result })
-} else {
+    } else {
     res.status(400).json({ error: 'Falha ao tentar atualizar o Título do livro'})
-}});
+    }});
 
 // Rota para atualizar o Autor do livro
 router.put('/bookAuthor', async (req, res) => {
     const result = await updateBookAuthor(' ',' ')                              // Recebe 2 parâmetros, Título do livro que deseja atualizar e Novo Autor para o livro.
-if(result){
+    if(result){
     res.status(200).json({ result })
-} else {
+    } else {
     res.status(400).json({ error: 'Falha ao tentar atualizar o Autor do livro'})
-}})
+    }});
 
 // Rota para atualizar a Sinopse do livro
 router.put('/bookSynopsis', async (req, res) => {
-    const result = await updateBookSynopsis(' ',' ')                            // Recebe 2 parâmetros, Título do livro que deseja atualizar e Nova Sinopse para o livro.
-if(result){
+    const result = await updateBookSynopsis('TypeScript for JavaScript Developers','Descubra como o TypeScript pode melhorar a qualidade do seu código JavaScript com tipagem estática, interfaces e recursos modernos de desenvolvimento. Este livro é projetado para desenvolvedores JavaScript que desejam adotar TypeScript e aprender suas melhores práticas para projetos robustos e escaláveis.')                            // Recebe 2 parâmetros, Título do livro que deseja atualizar e Nova Sinopse para o livro.
+    if(result){
     res.status(200).json({ result })
-} else {
+    } else {
     res.status(400).json({ error: 'Falha ao tentar atualizar a Sinopse do livro'})
-}})  
+    }});  
 
 
 
